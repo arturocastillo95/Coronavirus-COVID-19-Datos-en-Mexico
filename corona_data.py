d3b863b85ecab266b607c45cd72a51acef503df5
Arturo Castillo <arturguay@gmail.com>
2020-04-01 00:32:58 -0600

    Data update 31/03/2020

--- corona_data.py
+++ corona_data.py
@@ -33,7 +33,7 @@ def pageNum(s):
 start_time = time.time()
 
 #Get the PDF
-url = "https://www.gob.mx/cms/uploads/attachment/file/544266/Tabla_casos_positivos_COVID-19_resultado_InDRE_2020.03.30.pdf"
+url = "https://www.gob.mx/cms/uploads/attachment/file/544538/Tabla_casos_positivos_COVID-19_resultado_InDRE_2020.03.31.pdf"
 file = requests.get(url).content
 
 # #Create TEMP path
